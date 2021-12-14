32ece0da603aa1c7a969b4d73838e6acec363a93
CyberOzzie <75351069+CyberOzzie@users.noreply.github.com>
2021-12-14 12:58:33 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+let sensor = 0
+cuteBot.stopcar()
+basic.showLeds(`
+    # # . # #
+    # # . # #
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+let strip = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    sensor = cuteBot.ultrasonic(cuteBot.SonarUnit.Inches)
+    if (sensor < 3) {
+        cuteBot.motors(-20, -20)
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ff00)
+    } else {
+        cuteBot.stopcar()
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
+    }
 })
